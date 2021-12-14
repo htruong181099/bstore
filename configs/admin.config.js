821d861106fc54677316300e537000f1e2e97b15
Hoàng Trương <hoangtruong181099@gmail.com>
2021-12-14 15:48:04 +0700

    Delete configs directory

--- configs/admin.config.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = {
-    email: "EMAIL@GMAIL.COM",
-    firstname: "FIRSTNAME",
-    lastname: "LASTNAME",
-    phone: "PHONE",
-    password: "PASSWORD",
-    roles: "admin"
-}
--- configs/jwt.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    "secret": "xyzkey123abc"
-}
